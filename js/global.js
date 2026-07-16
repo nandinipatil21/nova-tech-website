35343d8cda063155d435d274eba0eb97c0f5581a
nandini <nandinipatil212008@gmail.com>
2026-07-16 14:04:44 +0530

    navbar is in progress

---
+++ js/global.js
@@ -0,0 +1,22 @@
+async function loadComponent(id, file) {
+  try {
+    const res = await fetch(file);
+    if (!res.ok) {
+      throw new Error(`Cannot load ${file}`);
+    }
+
+    const html = await res.text();
+    document.getElementById(id).innerHTML = html;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function initHeader() {
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  await loadComponent("navbar", "components/navbar.html");
+
+  initHeader();
+});
